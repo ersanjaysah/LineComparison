3943ba3c7aea590b6ed5f1c091e4f8d144fc6b69
sanjay <ssah6694@gmail.com>
2022-03-09 18:26:45 +0530

    [sanjay]added: length comprison

---
+++ Line_Comprison/Line_Comprison/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comprison
+{
+    public class Class1
+    {
+        double sum;
+        public void length(int x1, int y1, int x2, int y2)
+        {
+            sum = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("result is= " + sum);
+
+        }
+    }
+}
---
+++ Line_Comprison/Line_Comprison/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Line_Comprison
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+
+            Console.WriteLine("HELLO SIR Welcome to Line Comparision Computation Program");
+            Console.WriteLine("please enter your 1st cordinate number");
+            Class1 obj = new Class1();
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter your 2nd cordinate number");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter your 3rd cordinate number");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter your 4th cordinate number");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            obj.length(x1, x2, y1, y2);
+        }
+    }
+}
